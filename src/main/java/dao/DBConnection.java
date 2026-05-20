a262702b87f37b3110a5188b8c9822513bb6c4fe
Tamotsidd <tamotsidd7@gmail.com>
2026-05-20 11:45:59 +0545

    Fix registration backend: updated DB password and refactored UserDAO to use try-with-resources to prevent connection drop issues

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/slotsync";
     private static final String USER = "root";
-    private static final String PASSWORD = "20620816";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() {
         Connection con = null;
